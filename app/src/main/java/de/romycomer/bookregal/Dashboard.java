3dba89582a94a0ad8f96922c3ed45c7921dd1ee3
Omer Guimdo <omersucces@yahoo.com>
2021-06-02 13:31:53 +0200

    navigation Drawer

---
+++ app/src/main/java/de/romycomer/bookregal/Dashboard.java
@@ -0,0 +1,38 @@
+package de.romycomer.bookregal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+
+    //Initialize variable
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        //Assign variable
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+    public void ClickMenu(View view){
+        //open drawer
+        MainActivity.openDrawer(drawerLayout);
+    }
+    public void clickLogo(View view){
+        //close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+    public void ClickHome(View view){
+        //redirect Activity to home
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+    public void ClickDashboard(View view){
+        //recreate activity
+        recreate();
+    }
+}
--- app/src/main/java/de/romycomer/bookregal/MainActivity.java
+++ app/src/main/java/de/romycomer/bookregal/MainActivity.java
@@ -2,19 +2,77 @@ package de.romycomer.bookregal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    //initialize variable
 
     private Toolbar toolbar;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // assign variable
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    public void ClickMenu(View view){
+        //Open drawer
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        //open drawer layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    
+    public void ClickLogo(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        //close drawer layout
+        //check condition
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            //when drawer is open
+            //close drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public void ClickHome(View view){
+        //recreate activity
+        recreate();
+    }
+
+    public  void ClickDashboard(View view){
+        //redirect activity to dashboard
+        redirectActivity(this,Dashboard.class);
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        //Initialize intent
+        Intent intent = new Intent(activity,aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //start activity
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //close drawer
+        closeDrawer(drawerLayout);
     }
 }
